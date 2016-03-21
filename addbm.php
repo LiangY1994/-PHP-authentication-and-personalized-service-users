78cedcbbaeac19bc10d9ce5bd8577f1682fc1a6d
LiangY <liangyi19941213@gmail.com>
2016-03-21 17:46:40 +0100

    Function to add a bookmark

---
+++ addbm.php
@@ -0,0 +1,15 @@
+<?php
+    require 'bookmark_fns.php';
+    session_start();
+    html_header('Main Page');
+    html_siteInfo();
+    html_mainpage();
+    echo '<br />';
+    if(!$_SESSION['valid_name']){ // not define $_SESSION means not logged in
+        echo "<h2>You are not logged in.</h2>";
+        echo "<a href='login.php'>Click here to log in.</a><br />";
+    }
+    else{
+        addbookmarks();
+    }
+    html_footer();
